1f60d86ba9dec86135828c8fec7f079c34abb02e
Ben Force <benforce@gmail.com>
2017-08-06 09:25:58 -0500

    Adding sourcemap support

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,28 @@
 var gulp = require('gulp');
 var ts = require('gulp-typescript');
 var tsProject = ts.createProject('tsconfig.json');
+var sourcemaps = require('gulp-sourcemaps');
+var path = require('path');
 
 const exec = require('child_process').execSync;
+const sourceFiles = 'src/**/*.ts';
 
 const destination = './build';
 
 gulp.task('default', function() {
-    return gulp.src('src/**/*.ts')
+    return gulp.src(sourceFiles)
+        .pipe(sourcemaps.init())
         .pipe(tsProject())
-        .js.pipe(gulp.dest(destination));
+        .js
+        .pipe(sourcemaps.write({
+            sourceRoot: function(file) {
+                var sourceFile = path.join(file.cwd, file.sourceMap.file);
+                return path.relative(path.dirname(sourceFile), file.cwd);
+            }
+        }))
+        .pipe(gulp.dest(destination));
 });
 
-gulp.task('tests', ['default'], function() {
-    return gulp.src('tests/**/*.ts')
-        .pipe(tsProject())
-        .js.pipe(gulp.dest(destination));
+gulp.task('watch', ['default'], function() {
+    gulp.watch(sourceFiles, ['default'])
 });
